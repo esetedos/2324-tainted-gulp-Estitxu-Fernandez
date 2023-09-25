1e45f5f2730efd3b925a9c1c5d302f1f7058b8f5
esetedos <esther.fernandez@ikasle.aeg.eus>
2023-09-25 10:19:56 +0200

    [NEW] C.1 Creación de la clase character

---
+++ character.mjs
@@ -0,0 +1,19 @@
+export class Character{
+    constructor(fullName, health, magick, stamina, potions){
+        this.fullName = fullName;
+        this.health = health;
+        this.magick = magick;
+        this.stamina = stamina;
+        this.potions = potions;
+    }
+
+    static from(playerData, potions){
+        return new Character(
+            playerData.name + " the " + playerData.class,
+            playerData.health,
+            playerData.magick,
+            playerData.stamina,
+            potions
+        )
+    }
+}
